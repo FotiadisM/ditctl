eb4f4e875199ebee2c736cab6102af5b4f32f6b4
Fotiadis Michail <fotiadis.michalis20@gmail.com>
2021-04-16 21:22:18 +0300

    printing lessons

--- cmd/lessons.go
+++ cmd/lessons.go
@@ -22,13 +22,22 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 
 	"github.com/FotiadisM/ditctl/pkg/config"
 	"github.com/FotiadisM/ditctl/pkg/parser"
+)
+
+var (
+	verbose  bool
+	refresh  bool
+	output   string
+	semester int
 )
 
 // lessonsCmd represents the lessons command
@@ -46,7 +55,7 @@ to quickly create a Cobra application.`,
 		var sems []config.Semester
 		sems = config.GetSemesters()
 
-		if len(sems) == 0 {
+		if len(sems) == 0 || refresh {
 			var err error
 			sems, err = refreshCache()
 			if err != nil {
@@ -54,18 +63,28 @@ to quickly create a Cobra application.`,
 			}
 		}
 
-		printNormal(sems)
+		switch output {
+		case "yaml":
+		case "json":
+		default:
+			if verbose {
+				printVerbose(sems)
+				return
+			}
+			printNormal(sems)
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(lessonsCmd)
 
-	lessonsCmd.Flags().BoolP("verbose", "v", false, "verbose output")
-	lessonsCmd.Flags().BoolP("refresh", "r", false, "fetch lesson from the internet and update the cache")
-	lessonsCmd.Flags().IntP("semester", "s", -1, "only retrieve lessons of the given semester")
+	lessonsCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	lessonsCmd.Flags().BoolVarP(&refresh, "refresh", "r", false, "fetch lesson from the internet and update the cache")
+	lessonsCmd.Flags().IntVarP(&semester, "semester", "s", -1, "only retrieve lessons of the given semester")
 	lessonsCmd.Flags().Lookup("semester").DefValue = "all"
-	lessonsCmd.Flags().StringP("output", "o", "table", "output mode (table, yaml, json)")
+	lessonsCmd.Flags().StringVarP(&output, "output", "o", "table", "output mode (table, yaml, json)")
 }
 
 func refreshCache() (sems []config.Semester, err error) {
@@ -82,12 +101,41 @@ func refreshCache() (sems []config.Semester, err error) {
 }
 
 func printNormal(sems []config.Semester) {
-	table := tablewriter.NewWriter(os.Stdout)
+	var data [][]string
+	for i, s := range sems {
+		for _, l := range s.Lessons {
+			data = append(data, []string{strconv.Itoa(i+1) + "o", l.Name, l.Code, l.Ects})
+		}
+	}
 
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"ΕΞΑΜΗΝΟ", "ΜΑΘΗΜΑ", "ΚΩΔΙΚΟΣ", "ECTS"})
+	table.SetAutoMergeCellsByColumnIndex([]int{0})
+	table.SetCenterSeparator("|")
+	table.AppendBulk(data)
+	table.SetRowLine(true)
+	fmt.Println()
 	table.Render()
+	fmt.Println()
 }
 
 func printVerbose(sems []config.Semester) {
+	var data [][]string
+	for i, s := range sems {
+		for _, l := range s.Lessons {
+			data = append(data, []string{strconv.Itoa(i+1) + "o", l.Name, l.Code, l.Ects, l.Necessity, l.S1, l.S2, l.S3, l.S4, l.S5, l.S6})
+		}
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"ΕΞΑΜΗΝΟ", "ΜΑΘΗΜΑ", "ΚΩΔΙΚΟΣ", "ECTS", "ΥΠΟΧΡΕΩΤΙΚΟ", "S1", "S1", "S3", "S4", "S5", "S6"})
+	table.SetAutoMergeCellsByColumnIndex([]int{0})
+	table.SetCenterSeparator("|")
+	table.AppendBulk(data)
+	table.SetRowLine(true)
+	fmt.Println()
+	table.Render()
+	fmt.Println()
 
 }
 
